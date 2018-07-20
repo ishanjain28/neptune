daa383db5986d3aa9e3487ff6c9363f0ffeda38f
ishanjain28 <ishanjain28@gmail.com>
2018-07-20 20:46:26 +0530

    completed a basic bootable kernel in rust

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,18 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't change the name of function in symbol table
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
