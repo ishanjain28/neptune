c5ef2202a9c908af46c3e63efc3c0b347b467718
ishanjain28 <ishanjain28@gmail.com>
2019-05-19 14:33:33 +0530

    Added templates to support integration tests

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 // Utilise custom test frameworks feature in rust, Since the default one doesn't works in no_std environments
 #![feature(custom_test_frameworks)]
 // Specify the runner, aka, The function that will execute all the tests
-#![test_runner(crate::test_runner)]
+#![test_runner(neptune::test_runner)]
 // custom test frameworks creates an entry point called `main`. Since we are using no_main and have
 // set a custom entry point, The entry point created by custom test framework gets ignored.
 // This changes the name of the entry point to test_main which can then be called in _start
@@ -29,52 +29,18 @@ pub fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 #[panic_handler]
-pub fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-// u32 because iosize is set to 4 bytes
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
+fn panic(info: &PanicInfo) -> ! {
+    neptune::test_panic_handler(info)
 }
 
 #[no_mangle] // don't change the name of function in symbol table
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    println!(", some numbers: {} {}", 42, 1.337);
+    println!("Hello! Here are some numbers: {} {}", 42, 1.337);
 
     #[cfg(test)]
     test_main();
 
     loop {}
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        // iobase address
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    serial_println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
